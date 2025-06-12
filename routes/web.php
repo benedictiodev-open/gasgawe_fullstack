895877dc2abc7addc5b5f1b64f5bce61c89b6e58
yogaprianjaya <yogaprianjaya@gmail.com>
2025-06-13 06:31:37 +0700

    generate swagger artisan

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/artisan-master', function () {
+    Artisan::call('vendor:publish', [
+        '--provider' => 'L5Swagger\L5SwaggerServiceProvider'
+    ]);
+});
 Route::get('/artisan', function () {
     Artisan::call('l5-swagger:generate');
 });
