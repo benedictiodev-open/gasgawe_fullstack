3eee606fd74a22ec8f37e266c976b8e17686637a
yogaprianjaya <yogaprianjaya@gmail.com>
2025-06-13 06:06:20 +0700

    update get token google

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get_token_google', function () {
+    return view('get_token_google');
+});
