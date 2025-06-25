e4cccb8e2f874e9e25324ebbc3a19ef0de311941
Muhammad Esa Yusriana <esa.yusriana@bts.id>
2025-06-25 21:50:04 +0700

    feat(api-skill): add routes in api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\api\auth\AuthController;
 use App\Http\Controllers\api\Jobs\JobsController;
+use App\Http\Controllers\Api\Masterdata\SkillController;
 use App\Http\Middleware\AuthApiApplicant;
 use App\Http\Middleware\AuthApiChecker;
 use App\Http\Middleware\AuthApiRecruiter;
@@ -15,17 +16,17 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware(AuthApiChecker::class)->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-    Route::prefix('recruiter')->middleware(AuthApiRecruiter::class)->group(function() {
-        Route::prefix('/jobs')->group(function() {
+
+    Route::prefix('recruiter')->middleware(AuthApiRecruiter::class)->group(function () {
+        Route::prefix('/jobs')->group(function () {
             Route::get('/', [JobsController::class, 'list_job_reqruiter']);
             Route::get('/{id}/detail', [JobsController::class, 'detail_job']);
             Route::post('/create', [JobsController::class, 'add_job']);
         });
     });
 
-    Route::prefix('applicant')->middleware(AuthApiApplicant::class)->group(function() {
-        Route::prefix('/jobs')->group(function() {
+    Route::prefix('applicant')->middleware(AuthApiApplicant::class)->group(function () {
+        Route::prefix('/jobs')->group(function () {
             Route::get('/', [JobsController::class, 'list_job_applicant']);
             Route::get('/{id}/detail', [JobsController::class, 'detail_job']);
             Route::post('/bookmark', [JobsController::class, 'bookmark_job']);
@@ -35,3 +36,7 @@ Route::middleware(AuthApiChecker::class)->group(function () {
     });
 });
 
+
+Route::prefix('masterdata')->group(function () {
+    Route::get('/skill', [SkillController::class, 'skill']);
+});
